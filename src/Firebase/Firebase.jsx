fbe542037b83badce122c5fd42da25a754bd90ef
Irtaza Ahmed <irtazaahmedk@gmail.com>
2024-12-02 22:20:36 +0500

    basic structure and npm installing

---
+++ src/Firebase/Firebase.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUgAK0CV3h4Bc78MjN32G-4FFSh0-gplA",
+  authDomain: "chatapp-b0b76.firebaseapp.com",
+  projectId: "chatapp-b0b76",
+  storageBucket: "chatapp-b0b76.firebasestorage.app",
+  messagingSenderId: "159305042262",
+  appId: "1:159305042262:web:34f9058e379d69f1496ef7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
